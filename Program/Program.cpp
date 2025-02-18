b5ba2be9baa48ae541c640140f6bccf378e1ee3b
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-02-18 19:44:44 +0900

    (2025.2.18) 스택

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -25,11 +25,113 @@ public:
         size = 0;
     }
 
+    void push_front(T data)
+    {
+        Node * newNode = new Node;
+
+        newNode->data = data;
+        newNode->next = nullptr;
+        newNode->previous = nullptr;
+
+        if (head == nullptr)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            head->previous = newNode;
+            newNode->next = head;
+
+            head = newNode;
+        }
+
+        size++;
+    }
+
+    void pop_front()
+    {
+        if (head == nullptr)
+        {
+            cout << "Linked List is Empty" << endl;
+        }
+        else
+        {
+            Node* deleteNode = head;
+
+            if (head == tail)
+            {
+                head = nullptr;
+                tail = nullptr;
+            }
+            else
+            {
+                deleteNode->next->previous = nullptr;
+
+                head = head->next;
+            }
+
+            delete deleteNode;
+
+            size--;
+        }
+    }
+
+    void push_back(T data)
+    {
+        Node* newNode = new Node;
+
+        newNode->data = data;
+        newNode->next = nullptr;
+        newNode->previous = nullptr;
+
+        if (tail == nullptr)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            newNode->previous = tail;
+
+            tail = newNode;
+        }
+
+        size++;
+    }
+
+    void show()
+    {
+        Node* currentNode = head;
+
+        while (currentNode != nullptr)
+        {
+            cout << currentNode->data << " ";
+
+            currentNode = currentNode->next;
+        }
+    }
+
+    ~DoubleLinkedList()
+    {
+        while (head != nullptr)
+        {
+            pop_front();
+        }
+    }
+
 };
 
 int main()
 {
+    DoubleLinkedList<int> doubleLinkedList;
 
+    doubleLinkedList.push_front(30);
+    doubleLinkedList.push_front(20);
+    doubleLinkedList.push_front(10);
+
+    doubleLinkedList.show();
 
     return 0;
 }

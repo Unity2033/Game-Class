d88466f9107b8618df770347048d20e0a0e6fd53
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-02-19 19:44:37 +0900

    (2025.2.19) 선형 큐

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -3,135 +3,53 @@
 using namespace std;
 
 template <typename T>
-class DoubleLinkedList
+class CircleLinkedList
 {
 private:
     struct Node
     {
         T data;
-        Node * next;
-        Node * previous;
+        Node* next;
     };
 
-    Node* head;
-    Node* tail;
-
     int size;
+    Node * head;
+
 public:
-    DoubleLinkedList()
+    CircleLinkedList()
     {
-        head = nullptr;
-        tail = nullptr;
         size = 0;
-    }
-
-    void push_front(T data)
-    {
-        Node * newNode = new Node;
-
-        newNode->data = data;
-        newNode->next = nullptr;
-        newNode->previous = nullptr;
-
-        if (head == nullptr)
-        {
-            head = newNode;
-            tail = newNode;
-        }
-        else
-        {
-            head->previous = newNode;
-            newNode->next = head;
-
-            head = newNode;
-        }
-
-        size++;
-    }
-
-    void pop_front()
-    {
-        if (head == nullptr)
-        {
-            cout << "Linked List is Empty" << endl;
-        }
-        else
-        {
-            Node* deleteNode = head;
-
-            if (head == tail)
-            {
-                head = nullptr;
-                tail = nullptr;
-            }
-            else
-            {
-                deleteNode->next->previous = nullptr;
-
-                head = head->next;
-            }
-
-            delete deleteNode;
-
-            size--;
-        }
+        head = nullptr;
     }
 
     void push_back(T data)
     {
-        Node* newNode = new Node;
+        Node * newNode = new Node;
 
         newNode->data = data;
-        newNode->next = nullptr;
-        newNode->previous = nullptr;
 
-        if (tail == nullptr)
+        if (head == nullptr)
         {
             head = newNode;
-            tail = newNode;
+
+            newNode->next = head;
         }
         else
         {
-            tail->next = newNode;
-            newNode->previous = tail;
+            newNode->next = head->next;
 
-            tail = newNode;
+            head->next = newNode;
+
+            head = newNode;
         }
 
         size++;
-    }
-
-    void show()
-    {
-        Node* currentNode = head;
-
-        while (currentNode != nullptr)
-        {
-            cout << currentNode->data << " ";
-
-            currentNode = currentNode->next;
-        }
-    }
-
-    ~DoubleLinkedList()
-    {
-        while (head != nullptr)
-        {
-            pop_front();
-        }
     }
 
 };
 
 int main()
 {
-    DoubleLinkedList<int> doubleLinkedList;
-
-    doubleLinkedList.push_front(30);
-    doubleLinkedList.push_front(20);
-    doubleLinkedList.push_front(10);
-
-    doubleLinkedList.show();
 
     return 0;
 }

3b4b4e46d9e0b5b05718618a8e76839b9ee8c3aa
Unity2033 <82032086+Unity2033@users.noreply.github.com>
2025-03-03 19:44:46 +0900

    (2025.3.3) 완전 이진 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,119 +1,64 @@
 ﻿#include <iostream>
 
+#define SIZE 6
+
 using namespace std;
 
-class String
+template<typename KEY, typename VALUE>
+class HashTable
 {
 private:
-    int size;
-    char * pointer;
+    struct Node
+    {
+        KEY key;
+        VALUE value;
 
+        Node* next;
+    };
+
+    struct Bucket
+    {
+        int count;
+        Node* head;
+    };
+
+    Bucket bucket[SIZE];
 public:
-    String()
+    HashTable()
     {
-        size = 0;
-        pointer = nullptr;
-    }
-
-    void operator = (const char * word)
-    {
-        size = strlen(word) + 1;
-
-        if (pointer == nullptr)
+        for (int i = 0; i < SIZE; i++)
         {
-            pointer = new char[size];
-
-            for (int i = 0; i < size; i++)
-            {
-                pointer[i] = word[i];
-            }
-        }
-        else
-        {
-            char* newPointer = new char[size];
-
-            for (int i = 0; i < size; i++)
-            {
-                newPointer[i] = word[i];
-            }
-
-            delete [ ] pointer;
-
-            pointer = newPointer;
+            bucket[i].count = 0;
+            bucket[i].head = nullptr;
         }
     }
 
-    void Append(const char * word)
+    template <typename T>
+    const int & HashFunction(T key)
     {
-        int resize = size;
+        unsigned int hashIndex = (int)key % SIZE;
 
-        size = size + strlen(word) + 1;
-
-        char * newPointer = new char[size];
-
-        for (int i = 0; i < resize; i++)
-        {
-            newPointer[i] = pointer[i];
-        }
-
-        for (int i = 0; i < strlen(word) + 1; i++)
-        {
-            newPointer[resize + i] = word[i];
-        }
-
-         delete[] pointer;
-        
-        pointer = newPointer;
+        return hashIndex;
     }
 
-    const int& Size()
+    template<>
+    const int & HashFunction(const char * key)
     {
-        return size - 1;
-    }
+        int hash = 0;
 
-    const char& operator [] (const int& index)
-    {
-        return pointer[index];
-    }
-
-    ~String()
-    {
-        if (pointer != nullptr)
+        for (int i = 0; i < strlen(key); i++)
         {
-            delete [ ] pointer;
+            hash += key[i];
         }
+
+        int hashIndex = hash % SIZE;
+
+        return hashIndex;
     }
 };
 
 int main()
 {
-     // String string;
-     // 
-     // string = "key";
-     // 
-     // for (int i = 0; i < string.Size(); i++)
-     // {
-     //     cout << string[i];
-     // }
-     // 
-     // cout << endl;
-     // 
-     // string = "apple";
-     // 
-     // for (int i = 0; i < string.Size(); i++)
-     // {
-     //     cout << string[i];
-     // }
-     // 
-     // cout << endl;
-     // 
-     // string.Append(" Four");
-     // string.Append(" x");
-     // 
-     // for (int i = 0; i < string.Size(); i++)
-     // {
-     //     cout << string[i];
-     // }
 
      return 0;
 }
